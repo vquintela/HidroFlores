4c4983b63b0d3393eb4e725a93e753df1e923f86
vitor <vitor@vitor>
2020-05-31 19:35:31 -0300

    pagina31-05

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,12 +1,35 @@
-/* global fetch */
+document.getElementById("btn-menu").addEventListener("click", () => {
+    document.getElementById("navbar-nav").classList.toggle("show");
+});
 
-function myFunction() {
-  let x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+let slider = document.querySelector(".slider-contenedor")
+let sliderIndividual = document.querySelectorAll(".contenido-slider")
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 3000;
+
+window.addEventListener("resize", function(){
+    width = sliderIndividual[0].clientWidth;
+})
+
+setInterval(function(){
+    slides();
+},intervalo);
+
+
+
+function slides(){
+    slider.style.transform = "translate("+(-width*contador)+"px)";
+    slider.style.transition = "transform .8s";
+    contador++;
+
+    if(contador == sliderIndividual.length){
+        setTimeout(function(){
+            slider.style.transform = "translate(0px)";
+            slider.style.transition = "transform 0s";
+            contador=1;
+        },1500)
+    }
 }
 
 class Email{
